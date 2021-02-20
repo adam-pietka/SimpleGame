ba0da40be26a35cff988af63b9b77613536ea42d
Adam Pietka <adam.pietk@gmail.com>
2021-02-20 22:34:36 +0100

    second

--- black-jack/src/main/java/com/kodilla/blackjack/BlackJack.java
+++ black-jack/src/main/java/com/kodilla/blackjack/BlackJack.java
@@ -1,18 +1,25 @@
 package com.kodilla.blackjack;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class BlackJack extends Application {
 
 //    private Image imageback = new Image("file:src/main/resources/table.png");
-    private Image imageback = new Image("file:src/main/resources/black-jack.jpg");
+    private Image imageback = new Image("file:src/main/resources/BlackJack.png");
+    private  Image card2z = new Image("file:src/main/resources/2z.png");
+    private FlowPane cards = new FlowPane(Orientation.HORIZONTAL);
+    private FlowPane cards2 = new FlowPane(Orientation.HORIZONTAL);
+    private Button buttonYes = new Button("Yes");
 
     public static void main(String[] args) {
         launch(args);
@@ -25,7 +32,32 @@ public class BlackJack extends Application {
         Background background = new Background(backgroundImage);
 
         GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
         grid.setBackground(background);
+//        grid.add(new Button());
+
+
+        ImageView img = new ImageView(card2z);
+        img.setFitHeight(100);
+        img.setFitWidth(80);
+        cards.getChildren().add(img);
+        grid.add(cards, 0, 0,3,1);
+//        grid.add(cards, 0, 0,1,3);
+
+
+
+        ImageView img2 = new ImageView(card2z);
+        img2.setFitHeight(100);
+        img2.setFitWidth(80);
+        cards2.getChildren().add(img2);
+//        grid.add(cards, 0, 0,3,1);
+        grid.add(cards2, 1, 3,10,11);
+
+
+
 
         Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
 
